c058acefa784dcc33c4e67d933842f977249c16c
maiktanneberg <maiktanneberg@10.17.103.248>
2015-04-13 14:09:46 +0200

    Test Calender PHP Sonderzeichen cp1252

--- def_css/calendar.php
+++ def_css/calendar.php
@@ -7,20 +7,20 @@
  * @copyright 2007 i-fabrik GmbH
  * @version $Id: calendar.php,v 1.3 2007/02/21 13:50:50 heiko Exp $
  * 
- * Im Rahmen der Veranstaltung SoftwarequalitÃ¤t im SS 2015 des Studigang Wirstschaftsingenieurwesen
- * mit Fachrichtung Informationstechnik soll das Postuch ,das ursprÃ¼nglich von Erik Reuter von der 
+ * Im Rahmen der Veranstaltung Softwarequalität im SS 2015 des Studigang Wirstschaftsingenieurwesen
+ * mit Fachrichtung Informationstechnik soll das Postuch ,das ursprünglich von Erik Reuter von der 
  * UniversitÃ¤t Leipzig entwickelt wurde, auf die BedÃ¼rfnisse der EAH Jena angepasst werden.
  *  
  * Im Rahmen der Vorlesung wird sich Gedanken Ã¼ber einen Anforderungskatalog gemacht, der im Laufe der 
  * Zeit eingearbeitet werden soll. Die Anforderungen werden mit Hilfe des Webportal www.agilespecs.com
  * zusammengefasst und verwaltet. 
  * 
- * @author: Tobias MÃ¶ller, BjÃ¶rn Hoffmann, Maik Tanneberg
+ * @author: Tobias Möller, Björn Hoffmann, Maik Tanneberg
  */
 
 /**
- * array_merge - FÃ¼gt die Elemente von zwei oder mehr Arrays zusammen, indem die Werte des einenan das Ende
- * des vorherigen angehÃ¤ngt werden. Das daraus resultierende Array wird zurÃ¼ckgegeben.
+ * array_merge - Fängt die Elemente von zwei oder mehr Arrays zusammen, indem die Werte des einenan das Ende
+ * des vorherigen angehÃ¤ngt werden. Das daraus resultierende Array wird zurückgegeben.
  *
  * @param: $_FORMVARS
  *
@@ -42,7 +42,7 @@
 	$_FORMVARS['color'] = strip_tags($_FORMVARS['color']);
 
 /**
- * getcwd() - Gibt das aktuelle Arbeitsverzeichnis zurÃ¼ck und schreibt es in die Variable $verzeichnis
+ * getcwd() - Gibt das aktuelle Arbeitsverzeichnis zurück und schreibt es in die Variable $verzeichnis
  */	
 	$verzeichnis = getcwd();
 
@@ -53,7 +53,7 @@
     //chdir($verzeichnis);
     
 /**
-  * include_once bindet eine angegebene Datei ein und fÃ¼hrt sie als PHP-Skript aus. Dieses Verhalten
+  * include_once bindet eine angegebene Datei ein und führt sie als PHP-Skript aus. Dieses Verhalten
   * ist identisch zu include, mit dem einzigen Unterschied, dass die Datei, wenn sie bereits eingebunden
   * wurde, nicht erneut eingebunden wird. Wie der Name schon sagt, wird sie nur einmal eingebunden werden.
   *
@@ -78,8 +78,8 @@
 	$ausgabe = $tpl->outputStr();
 
 	// Klammern ersetzen '{' & '}' duerfen im template nicht vorhanden sein da sonst die templatebiliothek durcheinander kommt
-	$ausgabe = str_replace( "Â¦<Â¦", "{", $ausgabe);
-	$ausgabe = str_replace( "Â¦>Â¦", "}", $ausgabe);
+	$ausgabe = str_replace( "¦<¦", "{", $ausgabe);
+	$ausgabe = str_replace( "¦>¦", "}", $ausgabe);
 
 	// generierten inhalt ausgeben */
 	echo $ausgabe;
